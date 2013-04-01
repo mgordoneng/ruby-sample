534a76e1be85349848ab735d7e447d83d17f1456
Marc Gordon <mjg31337@gmail.com>
2013-03-31 19:13:40 -0700

    more progress

--- lib/ruby/ArticleRetriever.rb
+++ lib/ruby/ArticleRetriever.rb
@@ -4,12 +4,15 @@ class ArticleRetriever
   include HTTParty
 
   def initialize
-    @uri = "http://api.nytimes.com/svc/search/v1/article?format=json&query=title%3A%22global+warming%22+org_facet%3A%5BENVIRONMENTAL+PROTECTION+AGENCY%5D&rank=oldest&api-key=16e898c2bf28287404ebb8748fb91d7a:9:67498079"
+    @api_key = " 16e898c2bf28287404ebb8748fb91d7a:9:67498079"
+    @query = "title%3A%22global+warming%22+org_facet%3A%5BENVIRONMENTAL+PROTECTION+AGENCY%5D&rank=oldest"
+    @end_point = "http://api.nytimes.com/svc/search/v1/article"  
   end
 
   def read_articles_from_api
     #TODO validate response from API is 200
-    response = HTTParty.get(@uri)
+   uri = @end_point + "?format=json&query=" + @query + "&api-key=" + @api_key 
+   response = HTTParty.get(@uri)
   end
 
   def read_articles_from_file(file_name)
@@ -25,8 +28,8 @@ class ArticleRetriever
 
 end
 
-obj = ArticleRetriever.new
+#obj = ArticleRetriever.new
 #data =  obj.read_articles_from_api
 #obj.write_articles_to_file(data, "output")
-data = obj.read_articles_from_file("output")
-puts data
+#data = obj.read_articles_from_file("output")
+#puts data
